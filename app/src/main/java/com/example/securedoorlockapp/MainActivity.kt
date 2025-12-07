3eb910958b710b5a7352bc7a2eaedf34be0b4cdc
Adam Cassidy <D00265095@student.dkit.ie>
2025-12-07 18:10:37 +0000

    mainactv

--- app/src/main/java/com/example/securedoorlockapp/MainActivity.kt
+++ app/src/main/java/com/example/securedoorlockapp/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.securedoorlockapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import com.example.securedoorlockapp.navigation.AppNav
+import com.example.securedoorlockapp.navigation.AppNavHost
+import com.example.securedoorlockapp.ui.theme.SecureDoorLockTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
+            SecureDoorLockTheme {
                 AppNav()
             }
         }
